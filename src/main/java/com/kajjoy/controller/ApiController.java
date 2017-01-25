0b54d0b523b61375651947ac06535b34ae022011
Chattopadyay, Dipayan (DS) <Dipayan.Chattopadyay@cdk.com>
2017-01-25 14:16:17 -0800

    first commit

---
+++ src/main/java/com/kajjoy/controller/ApiController.java
@@ -0,0 +1,23 @@
+package com.kajjoy.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+import java.util.UUID;
+
+@RestController
+public class ApiController {
+
+    @GetMapping("/")
+    String uid(HttpSession session) {
+        UUID uid = (UUID) session.getAttribute("uid");
+        if (uid == null) {
+            uid = UUID.randomUUID();
+        }
+        session.setAttribute("uid", uid);
+        return uid.toString();
+    }
+
+
+}
